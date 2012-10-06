dc206922be1c7ab0e40b268b5a03a3a0c78c7222
Dmitry Manayev <Dmitry.Manayev@hotmail.com>
2012-10-06 15:12:44 +0300

    Added zoom feature.

--- Ajapaik/AJTableViewController.m
+++ Ajapaik/AJTableViewController.m
@@ -118,13 +118,8 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
+    AJPhoto *photo = [_photos objectAtIndex:indexPath.row];
+    [self.delegate photoChoosen: photo];
 }
 
 #pragma mark - Location Manager
