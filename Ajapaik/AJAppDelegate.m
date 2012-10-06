7150e7cc48f52c1001ed597fd79ec232447033b3
Taavi Hein <taavi.hein@mobisolutions.com>
2012-10-06 10:47:56 +0300

    Code indenting cleanup...

--- Ajapaik/AJAppDelegate.m
+++ Ajapaik/AJAppDelegate.m
@@ -15,10 +15,10 @@
 {
 	UIViewController *viewController = [[AJMapViewController alloc] initWithNibName:@"AJMapViewController" bundle:[NSBundle mainBundle]];
 	
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+  self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 	self.window.rootViewController = viewController;
-    [self.window makeKeyAndVisible];
-    return YES;
+  [self.window makeKeyAndVisible];
+  return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
@@ -29,7 +29,7 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
 	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
